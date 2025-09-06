cf2116521d1bae037449e91b2cb14ab190a98fa9
shekofaamiri <shekofaamiri1@gmail.com>
2025-09-06 09:07:08 +0330

    Add if condition

--- main.cpp
+++ main.cpp
@@ -7,6 +7,16 @@ int main() {
     double a, b, c, d,x, x1, x2;
     cout << "Enter a,b,c";
     cin >> a >> b >> c;
+    if (a!=0)
+    {
+        d = b * b - 4 * a * c;
+        if (d > 0)
+        {
+            x1 = (-b + sqrt(d)) / (2 * a);
+            x2 = (-b - sqrt(d)) / (2 * a);
+            cout << "x1=" << x1 << endl;
+            cout << "x2=" << x2;
+        }
     return 0;
 
 }
